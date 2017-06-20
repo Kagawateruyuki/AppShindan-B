fc4fbe31f12e5d3fe8bf13627f270a42a92c135b
Sato Daichi <daichi.sato.sys@gmail.com>
2017-06-20 10:16:03 +0900

    first commit

---
+++ src/Shindan.java
@@ -0,0 +1,32 @@
+
+public class Shindan {
+	public static void main(String args[]){
+		//メイン文中里担当
+		//回答内容で診断をする処理
+		//回答結果を表示する処理
+	}
+
+	public void situmon1(){
+		//裕哉（田所）担当
+		//質問内容を表示
+		//回答内容を表示（１：男性２：女性）
+		//ユーザーが答えを入力
+	}
+
+	public void situmon2(){
+		//中里担当
+	}
+	public void situmon3(){
+		//葛西担当
+	}
+	public void situmon4(){
+		//大地担当
+	}
+	public void situmon5(){
+		//まるちゃん担当
+	}
+	public void situmon6(){
+		//ヒロキ担当
+	}
+
+}
